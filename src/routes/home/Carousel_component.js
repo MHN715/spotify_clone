8512c6579797d3363bfa547808edb5b7d9e3d880
MHN715 <nielsen.177@gmail.com>
2022-08-11 18:40:50 +0200

    tracks array has now been sorted, no more dublicates. using reduce and find method

--- src/routes/home/Carousel_component.js
+++ src/routes/home/Carousel_component.js
@@ -1,34 +1,28 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 
 export default function Carousel_component({ tracks }) {
-  // console.log(tracks);
-  const array = [
-    { id: 1, val: "hello 1a" },
-    { id: 1, val: "hello 1b" },
-    { id: 2, val: "hello 2a" },
-    { id: 2, val: "hello 2b" },
-  ];
-  // console.log("unsorted", array);
+  const [sortedTracks, setSortedTracks] = useState([]);
+  // console.log("unsorted", tracks);
 
   useEffect(() => {
-    const lol = tracks.filter((currentValue, index, arr) => {
-      // console.log(currentValue, index, arr);
-      // return currentValue.track.id !== "4mREn78o8CC6NUgOWnu34C";
-      // console.log(currentValue.track.id, arr[index].track.id);
-      // console.log(arr[index]);
+    if (tracks.length < 1) return;
 
-      // return currentValue.track.id === arr[index].track.id;
-      return arr.indexOf(currentValue.track.id) !== arr[index].track.id;
-    });
-    lol.map((item) => {
-      console.log(item.track.id);
-    });
+    const result = tracks.reduce((sortedArray, currentObj) => {
+      let dublicateObj = sortedArray.find(
+        (item) => item.track.id === currentObj.track.id
+      );
+      if (dublicateObj) return sortedArray;
+
+      return sortedArray.concat([currentObj]);
+    }, []);
+
+    setSortedTracks(result);
   }, [tracks]);
 
   return (
@@ -45,10 +39,10 @@ export default function Carousel_component({ tracks }) {
           height: 10rem;
         `}
       >
-        {tracks.map((item) => {
+        {sortedTracks.map((item) => {
           // console.log(item.track);
           const { id } = item.track;
-          return <SwiperSlide>lol</SwiperSlide>;
+          return <SwiperSlide key={id}>lol</SwiperSlide>;
         })}
       </Swiper>
     </div>
